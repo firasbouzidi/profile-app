a510573dde78d095c4ba4b37205070586932f087
firas bouzidi <firasbouzidi02@gmail.com>
2023-05-08 15:24:45 +0100

    profile-app

---
+++ src/profile/Adress.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Address() {
+  return (
+    <div>
+      <p>4131 23 St</p>
+      <p>Ezzouhour, Tun 12345</p>
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfilePhoto() {
+  return (
+    <div>
+      <img src="" alt="Profile" />
+    </div>
+  );
+}
+
+export default ProfilePhoto;
